0daae75c607caafdb1258b397aeaf8d5ab6b7e8b
Allef Douglas <allefwalker73@gmail.com>
2021-07-28 09:56:29 -0300

    version 0.0.1

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (request, response) => {
-  return response.send("Hello, world");
+  return response.send("Hello, world - Version 0.0.1");
 });
 
 app.listen(PORT, () => console.log(`App running at http://localhost:${PORT}`));
